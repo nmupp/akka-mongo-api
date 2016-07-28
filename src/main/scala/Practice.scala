558f2e639ff3c59da60766cf6de5e3d42e59a770
nmupp <nmupphere@gmail.com>
2016-07-28 17:10:01 -0500

    Scala practice

---
+++ src/main/scala/Practice.scala
@@ -0,0 +1,71 @@
+/**
+  * Created by nmupp on 7/28/16.
+  */
+class PracticeScala {
+
+  sealed trait List[+A]
+  case object Nil extends List[Nothing]
+  case class Cons[+A](head:A, tail:List[A]) extends List[A]
+
+  object List {
+    def apply[A](values: A*) :List[A] = {
+      if(values.isEmpty) Nil
+      else Cons(values.head,apply(values.tail:_*))
+    }
+
+    def sum[A <: Int](a:List[A]):Int = a match {
+      case Nil => 0
+      case Cons(x,xs) => x + sum(xs)
+    }
+
+    def tail[A](a:List[A]):List[A] = a match {
+      case Nil => a
+      case Cons(x,xs) => xs
+    }
+
+    def setHead[A](l:List[A],a:A):List[A] = l match {
+      case Cons(x,xs) => Cons(a,xs)
+    }
+
+    def drop[A](l:List[A],n:Int):List[A] = {
+      def use[A](l1:List[A],n1:Int):List[A] = {
+        if (n1 == 0)
+          l1
+        else {
+          l1 match {
+            case Nil => l1
+            case Cons(x,xs) => use(xs,n1-1)
+          }
+        }
+      }
+      use(l,n)
+    }
+
+    def append[A](l1:List[A],l2:List[A]):List[A] = l1 match {
+      case Nil => l2
+      case Cons(x,xs) => Cons(x,append(xs,l2))
+    }
+
+    def dropWhile[A](l:List[A], f:A => Boolean):List[A] = l match {
+      case Cons(x,xs) => if(f(x)) dropWhile(xs,f) else l
+    }
+  }
+
+  class SuperMe {
+    def apply() = {}
+  }
+
+  case class Child() extends SuperMe
+
+  val list:List[SuperMe] = List(Child())
+
+  List.sum(List(1,2,3,7))
+
+  List.drop(List(1,2,3,4),1)
+
+  List.dropWhile(List(1,2,3,4), (a:Int) => {a == 2})
+
+  List.append(List(1,2,3,4),List(5,6,7))
+
+}
+
